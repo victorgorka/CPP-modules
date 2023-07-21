3be244a288538154ea6d699c6b53ac7fa5e5f4d1
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-07-21 12:39:14 +0200

    cpp01/ex06 finished

---
+++ CPP01/ex06/Harl.hpp
@@ -0,0 +1,22 @@
+#ifndef HARL_HPP
+# define HARL_HPP
+
+// includes
+# include <string>
+# include <iostream>
+
+// class
+class Harl
+{
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+	public:
+		void	complain(std::string level);
+		Harl();
+		~Harl();
+};
+
+#endif
