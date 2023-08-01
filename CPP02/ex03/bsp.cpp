66004986f39e632b5679c0156799b9297727c68b
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-08-01 10:40:14 +0200

    new file bsp.cpp in cpp02/ex03

---
+++ CPP02/ex03/bsp.cpp
@@ -0,0 +1,25 @@
+#include "Point.hpp"
+#include <cmath>
+
+static float	area(Point const &x, Point const &y, Point const &z)
+{
+	return abs((x.getX() * (y.getY() - z.getY()) + y.getX() * (z.getY() - x.getY())
+		+ z.getX() * (x.getY() - y.getY())) / 2);
+}
+
+bool	bsp( Point const a, Point const b, Point const c, Point const point)
+{
+	float	abc, pab, pbc, pac;
+
+	abc = area(a, b, c);
+	pab = area(point, a, b);
+	pbc = area(point, b, c);
+	pac = area(point, a, c);
+
+	if (pab == 0 || pbc == 0 || pac == 0)
+		return false;
+	else if (pab + pbc + pac == abc)
+		return true;
+	else
+		return false;
+}
