b78e1564e956ec220104797d23519bd68beda3a8
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-08-02 19:16:57 +0200

    cpp03/ex01 finished

---
+++ CPP03/ex01/ScavTrap.hpp
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-prad <vde-prad@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/02 16:34:01 by vde-prad          #+#    #+#             */
+/*   Updated: 2023/08/02 16:34:02 by vde-prad         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include <iostream>
+# include <string>
+# include "ClapTrap.hpp"
+
+class ScavTrap: public ClapTrap
+{
+	public:
+		// Constructors
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap &copy);
+		
+		// Destructor
+		~ScavTrap();
+		
+		// Operators
+		ScavTrap & operator=(const ScavTrap &assign);
+
+		// Methods
+		void	guardGate();
+
+	private:
+		ScavTrap();
+		
+};
+
+#endif
