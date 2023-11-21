ce5d23ef2773b17c1e541fa6c4bd32a8ae9c670e
victor <vde-prad@student.42malaga.com>
2023-11-21 23:52:31 +0100

    ex00 files stored in directory, ex01 directory created

---
+++ CPP05/ex01/Bureaucrat.cpp
@@ -0,0 +1,80 @@
+#include "Bureaucrat.hpp"
+
+// Exceptions
+class Bureaucrat::GradeTooHighException: public std::exception
+{
+	virtual const char *what() const throw()
+	{
+		return "Grade Too High";
+	}
+};
+
+class Bureaucrat::GradeTooLowException: public std::exception
+{
+	virtual const char *what() const throw()
+	{
+		return "Grade Too Low";
+	}
+};
+
+// Constructors
+Bureaucrat::Bureaucrat()
+{
+	std::cout << "\e[0;33mDefault Constructor called of Bureaucrat\e[0m" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &copy)
+{
+	(void) copy;
+	std::cout << "\e[0;33mCopy Constructor called of Bureaucrat\e[0m" << std::endl;
+}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade) : _name(name), _grade(grade)
+{
+	std::cout << "\e[0;33mConstructor called of Bureaucrat\e[0m" << std::endl;
+	if (grade < 1)
+		throw GradeTooHighException();
+	else if (grade > 150)
+		throw GradeTooLowException();
+}
+
+
+// Getters
+const std::string	Bureaucrat::getName() const		{return _name;}
+int					Bureaucrat::getGrade() const	{return _grade;}
+
+// Methods
+void Bureaucrat::incrementGrade(void)
+{
+	this->_grade--;
+	if (_grade < 1)
+		throw GradeTooHighException();
+}
+
+void Bureaucrat::decrementGrade(void)
+{
+	this->_grade++;
+	if (_grade > 150)
+		throw GradeTooLowException();
+}
+
+// Destructor
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "\e[0;31mDestructor called of Bureaucrat\e[0m" << std::endl;
+}
+
+
+// Operators
+Bureaucrat & Bureaucrat::operator=(const Bureaucrat &assign)
+{
+	// implementar la sobrecarga
+	(void) assign;
+	return *this;
+}
+
+std::ostream &operator<<(std::ostream &os, const Bureaucrat &obj)
+{
+	os << obj.getName() << ", bureaucrat grade " << obj.getGrade() << std::endl;
+	return os;
+}
