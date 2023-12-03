1fb1f82c676d506e74b385e264fd39f5b39eb1c6
victor <vde-prad@student.42malaga.com>
2023-12-03 21:06:58 +0100

    Problem compilation with undefined type fixed
    The namespace of the class was forgotten to be added in the
    implementation of both functions

--- CPP06/ex01/Serializer.cpp
+++ CPP06/ex01/Serializer.cpp
@@ -1,12 +1,12 @@
 #include "Serializer.hpp"
 
 // Methods
-uintptr_t	serialize(Data* ptr)
+uintptr_t	Serializer::serialize(Data* ptr)
 {
 	return reinterpret_cast<uintptr_t>(ptr);
 }
 
-Data		*deserialize(uintptr_t raw)
+Data		*Serializer::deserialize(uintptr_t raw)
 {
 	return reinterpret_cast<Data *>(raw);
 }
--- CPP06/ex01/main.cpp
+++ CPP06/ex01/main.cpp
@@ -3,11 +3,17 @@
 
 int main()
 {
+	// Original Data structure setted with a value
 	Data data;
 	data.num = 42;
-	Data *data2 = &data;
-	Data *data3 = NULL;
+	
+	// Data structure pointer in form of uintptr_t
+	uintptr_t ptrData = Serializer::serialize(&data);
 
-	std::cout << "Data num before serializer: " << data.num << std::endl;
-	data3 = Serializer::deserialize(Serializer::serialize(data2));
+	// Second Data structure that will recieve ptrData deserialized
+	Data *data2 = Serializer::deserialize(ptrData);
+
+	// Showing content of data2
+	std::cout << "num of data2: " << data2->num << std::endl;
+	return 0;
 }
