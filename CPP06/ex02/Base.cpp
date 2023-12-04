a5daad95b277b425a71365663f911cf1bdb9c675
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-12-04 19:57:23 +0100

    cpp06/ex02

---
+++ CPP06/ex02/Base.cpp
@@ -0,0 +1,6 @@
+#include "Base.hpp"
+
+// Destructor
+Base::~Base()
+{
+}
---
+++ CPP06/ex02/C.hpp
@@ -0,0 +1,12 @@
+#ifndef C_HPP
+# define C_HPP
+
+# include <iostream>
+# include <string>
+# include "Base.hpp"
+
+class C : public Base
+{
+};
+
+#endif
