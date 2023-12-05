7f29b46dcf733420c996b806c44d967b1e34d941
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-12-05 15:26:46 +0100

    cpp07/ex01 finished

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,10 +1,22 @@
 #ifndef ITER_HPP
 # define ITER_HPP
 
-template<typename T, typename U>
-void	iter(T *array, int len, U fun)
-{
+# include <iostream>
+# include <cstring>
 
+template<typename T, typename L>
+void	iter(T *array, L len, void (*ft)(T&))
+{
+	L i = 0;
+	while (i < len)
+		ft(array[i++]);
 }
 
+template<typename T, typename L>
+void	iter(T *array, L len, void (*ft)(T const &))
+{
+	L i = 0;
+	while (i < len)
+		ft(array[i++]);
+}
 #endif
--- CPP07/ex01/main.cpp
+++ CPP07/ex01/main.cpp
@@ -1,53 +1,13 @@
-#include <iostream>
-#include <Array.hpp>
+#include "Iter.hpp"
 
-#define MAX_VAL 750
-int main(int, char**)
+void	ft_uppercase(char &character)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	std::cout << (char)std::toupper(character);
+}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+int main(void)
+{
+	char s[] = "hola Mundo";
+	::iter(s, std::strlen(s), &ft_uppercase);
+	return 0;
 }
