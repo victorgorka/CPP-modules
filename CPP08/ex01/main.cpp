f55dc9bed837b19e061cb3121f8dd50e3b4da0ae
Victor Gorka De Prada Arnedo <vde-prad@student.42malaga.com>
2023-12-21 20:33:53 +0100

    test in ex01 fixed

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -2,7 +2,26 @@
 
 int main(void)
 {
-	std::cout << "TEST 1" << std::endl;
+	std::cout << std::endl << "****** SUBJECT TEST ******" << std::endl;
+	{
+		Span set = Span(5);
+
+		try
+		{
+			set.addNumber(6);
+			set.addNumber(3);
+			set.addNumber(17);
+			set.addNumber(9);
+			set.addNumber(11);
+			std::cout << "shortest span: " << set.shortestSpan() << std::endl;
+			std::cout << "longest span: " << set.longestSpan() << std::endl;
+		}
+		catch(std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+	}
+	std::cout << "****** OWN TEST ******" << std::endl;
 	{
 		std::vector<int> numbers;
 		Span set = Span(15);
@@ -15,30 +34,11 @@ int main(void)
 		numbers.push_back(-3);
 		numbers.push_back(7);
 		numbers.push_back(9);
-		std::cout << std::endl << "****** Se añaden 8 numeros ******" << std::endl;
+		std::cout << std::endl << "Se añaden 8 numeros" << std::endl;
 		try
 		{
 			set.addRange(numbers);
 			std::cout << "set size: " << set.getNumbers().size() << std::endl;
-			std::cout << "shortest span: " << set.shortestSpan() << std::endl;
-			std::cout << "longest span: " << set.longestSpan() << std::endl;
-		}
-		catch(std::exception &e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-	}
-	std::cout << std::endl << "TEST 2" << std::endl;
-	{
-		Span set = Span(5);
-
-		try
-		{
-			set.addNumber(6);
-			set.addNumber(3);
-			set.addNumber(17);
-			set.addNumber(9);
-			set.addNumber(11);
 			std::cout << "shortest span: " << set.shortestSpan() << std::endl;
 			std::cout << "longest span: " << set.longestSpan() << std::endl;
 		}
